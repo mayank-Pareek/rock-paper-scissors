6237b04cf853f2f15478711cef3a82d7719fbf5c
Mayank Kumar <mpareek365@gmail.com>
2022-11-01 02:27:56 -0400

    add playRound function created function to play a single round of the game

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ let choice = prompt("Please enter your choice");
 return choice.toLowerCase();
 };
 
+let playRound = function(){
+    let com = getComChoice();
+    let human = getHumanChoice();
+    console.log(`You chose ${human} and computer chose ${com}`);
+    if(com===human){
+      return "tie";
+    }
+    else if(com==="rock" && human ==="scissor"||com==="scissor" && human ==="paper"||com==="paper" && human ==="rock"){
+  return "com";
+    }
+     else return "human";
+  };
+  
+ 
+
